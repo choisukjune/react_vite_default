78dd0fa4a0bfa066e21cada4830d7f968f1a1f3f
choisukjune <sukjune.choi@gmail.com>
2023-09-06 19:37:40 +0900

    test

---
+++ krampoline/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig, loadEnv } from 'vite'
+
+import react from '@vitejs/plugin-react'
+
+const env = loadEnv( process.cwd(), '')
+
+const staticServerUri = env.VITE_A || "";  
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: `/${staticServerUri}/`,
+  plugins: [react()],
+})
