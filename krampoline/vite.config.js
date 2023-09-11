07ba231711846285455b32fbbb496665f7b61111
sukjune.choi <sukjune.choi@gmail.com>
2023-09-11 15:59:36 +0900

    ㅅㄷㄴㅅㄴ

--- krampoline/vite.config.js
+++ krampoline/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
+import path from "path";
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
+
 const env = loadEnv( process.cwd(), '')
 
 const staticServerUri = env.VITE_A || "";  
@@ -9,9 +10,12 @@ const staticServerUri = env.VITE_A || "";
 export default defineConfig({
   //base: `/${staticServerUri}`,
   plugins: [react()],
-    resolve: {
-        alias: {
-          "@@": path.resolve(__dirname, "./src/"),
-        },
+        build: {
+        rollupOptions: {
+            external: [
+                "react", // ignore react stuff
+                "react-dom",
+            ],
+        }
     },
 })
