4f0d744a77ab004d680f586d9e0e0c3e4871271e
sukjune.choi <sukjune.choi@gmail.com>
2023-10-10 16:46:56 +0900

    dfgdfg

--- krampoline/src/main.jsx
+++ krampoline/src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/" + import.meta.env.VITE_A}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
