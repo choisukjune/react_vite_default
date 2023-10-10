ac4eb8a2162b62998dc3a7c136c200f7be4567a3
sukjune.choi <sukjune.choi@gmail.com>
2023-10-10 16:40:11 +0900

    test

--- krampoline/vite.config.js
+++ krampoline/vite.config.js
@@ -2,16 +2,16 @@ import * as path from "path";
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const env = loadEnv( process.cwd(), '')
+// const env = loadEnv( process.cwd(), '')
 
-const staticServerUri = env.VITE_A || "";  ;
+// const staticServerUri = env.VITE_A || "";  ;
 
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/${staticServerUri}/`,
+  // base: `/${staticServerUri}/`,
   server: {
     port: 3000,
   },
